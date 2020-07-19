e2b780a6997bdd4682b56bd080f0bd5e667ee5e7
LorenaMendes <lorenamendes.contato@gmail.com>
2020-07-18 21:04:20 -0300

    Menu bar component added

---
+++ src/components/MenuBar/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    /* justify-content: 'space-around'; */
+    align-items: center;
+    background: rgb(73, 79, 82);
+    color: white;
+`;
+
+export const NavLink = styled.li`
+    list-style: none;
+    padding: 10px;
+    margin: 10px;
+`;
